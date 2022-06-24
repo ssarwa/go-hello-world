f653516284fee7b7164ebe712e67d47b681bb84e
Srikant Sarwa <srikantsarwa@outlook.com>
2022-06-24 20:21:49 +0530

    Updated code to introduce third party packages

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM golang:1.18-buster AS builder
+FROM golang:1.18-alpine AS builder
 WORKDIR /app
 COPY go.* ./
 RUN go mod download
 COPY *.go ./
-RUN go build -o /go-hello-world
+RUN go get 
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /go-hello-world
+#RUN go build -o /go-hello-world
 # Create a new release build stage
 FROM gcr.io/distroless/base-debian10
 # Set the working directory to the root directory path
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
-package main;
+package main
+
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
+	"github.com/sirupsen/logrus"
 )
+
 func main() {
-    http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hello, World!")
-    })
-    fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
+	var log = logrus.New()
+	log.Out = os.Stdout
+	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
